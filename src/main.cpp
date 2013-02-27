207e2f0e822e6c431c4fb054bb702dbfccf8d782
Cecilia <shuxiu.zhou@gmail.com>
2013-02-27 10:47:41 -0500

    modify main()

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,10 @@ int main(int argc, char *argv[])
 
 	if(argc < 2)
 	{
-		cerr << "Usage: lex_test pascal_file_name.\n";
+		cerr << "Usage: " << argv[0] << " pascal_file_name.\n";
 		return 1;
 	}
 
-
-/*
-	Scanner my_scanner(argv[1]);
-	my_scanner.scan();
-	my_scanner.print_lexeme_tokens();
-*/
 
     Parser my_parser(argv[1]);
     my_parser.parse();
